c9553fca90a070c8fc1381367b90a9310432df8b
manelzaabi <manel.zaabi@esprit.tn>
2023-10-19 12:27:58 +0100

    update appDataBAse

--- app/src/main/java/tn/esprit/nascar/utils/AppDatabase.kt
+++ app/src/main/java/tn/esprit/nascar/utils/AppDatabase.kt
@@ -1,25 +1,27 @@
 package tn.esprit.nascar.utils
 
+import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
 //TODO 10 Change this class to an abstract class that inherit from ROOMDatabase
-@Database(entities = [Events::class], version =1 )
-abstract class AppDatabase:RoomDatabase{
+@Database(entities = [Events::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun eventDao(): EventDao
 
 
-    companion object{
+    companion object {
         //TODO 11 Apply the Design Pattern singleton
-    priavte lateint var instance : appDatabase
-    fun getInstance(contexte: context): AppDataBase{
-        if(instance == null)
-            instance = Room
-                .databaseBuilder(context, AppDatabase:: class , "nascarDB")
-                .build
-        return instance
-    }
+        private lateinit var instance: AppDatabase
+
+        fun getInstance(context: Context): AppDatabase {
+            if (instance == null)
+                instance = Room
+                    .databaseBuilder(context.applicationContext, AppDatabase::class.java, "nascarDB")
+                    .build
+            return instance
+        }
 
     }
 }
